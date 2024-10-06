a30e13ff77b3c923d3943a5d36fb7536545d630f
mkaksoy <mehmetkaanaksoy13@gmail.com>
2024-10-06 22:21:31 +0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen min-h-screen flex justify-center items-center text-6xl font-semibold">
+      <h1 className="text-slate-950">Welcome to next-mini! ❤️</h1>
+    </div>
+  );
+};
+
+export default Home;
